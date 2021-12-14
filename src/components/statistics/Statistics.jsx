5ec78cb216aa8e5af599dcdcaa84d9a2e58bcfd6
IlliaBachkovskyi <illiabachkovskyi@gmail.com>
2021-12-14 07:41:37 +0200

    Update Statistics.jsx
    Добавил функцию generateRandomColor

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,13 +1,24 @@
 import PropTypes from 'prop-types';
 import styles from './Statistics.module.css';
 
+const generateRandomColor = () => {
+  let r = Math.round(Math.random() * 255);
+  let g = Math.round(Math.random() * 255);
+  let b = Math.round(Math.random() * 255);
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+};
+
 const Statistics = ({ title, stats }) => (
   <section className={styles.statistics}>
     <h2 className={styles.title}>{title}</h2>
 
     <ul className={styles.stat_list}>
       {stats.map(stat => (
-        <li className={styles.item} key={stat.id}>
+        <li
+          className={styles.item}
+          key={stat.id}
+          style={{ backgroundColor: generateRandomColor(stat) }}
+        >
           <span className={styles.label}>{stat.label}</span>
           <span className={styles.percentage}>{stat.percentage}%</span>
         </li>
